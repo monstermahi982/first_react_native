da9e6e8b206c64c39addef0a10eaf5b9c29d4594
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-05-12 12:39:44 +0530

    day1

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [value, setValue] = useState(0);
+  const fireEvent = () => {
+    setValue(value + 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{value}</Text>
+      <Button
+        title="monster"
+        color="red"
+        onPress={fireEvent}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
